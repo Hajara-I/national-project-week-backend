226752155e5dfe6ab5102d3fc9e15dde3fb2765c
Hajara Iyal <hajaraiyal@gmail.com>
2022-01-18 13:42:17 +0000

    heroku and files set up

---
+++ db/index.js
@@ -0,0 +1,22 @@
+import pg from "pg";
+
+import {
+	databaseHost,
+	databaseName,
+	dbPort,
+	password,
+	username,
+} from "../config.js";
+
+const pool = new pg.Pool({
+	user: username,
+	host: databaseHost,
+	database: databaseName,
+	password: password,
+	port: dbPort,
+	ssl: { rejectUnauthorized: false },
+});
+
+export default function query(text, params) {
+	return pool.query(text, params);
+}
---
+++ plan.md
@@ -0,0 +1,16 @@
+1. npx express-generator
+
+2. npm i -y
+
+3. gitignore
+
+4. file structure
+   // db folder containing scripts - createtable, populate table
+   // models - for functions
+   // routes
+   // .env - environment variables for protecitng API keys
+   // config file
+
+5. npm i pg
+
+6. npm i dotenv

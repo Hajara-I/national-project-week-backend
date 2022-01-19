7fe4863d617fc9ff182cf016a5573edf2f6845b4
AsmaDage <asmadage@gmail.com>
2022-01-19 11:38:26 +0000

    all routes done and tested via postman

--- models/workshopTracker.js
+++ models/workshopTracker.js
@@ -4,3 +4,29 @@ export async function getAllData() {
   const data = await query("SELECT * FROM workshopTracker");
   return data.rows;
 }
+
+export async function getById(id) {
+  const data = await query("SELECT * FROM workshopTracker WHERE id = $1", [id]);
+  return data.rows;
+}
+
+export async function postWorkshop(name, date, progress, mood) {
+  const data = await query(
+    "INSERT INTO workshopTracker (name, date,progress, mood) VALUES ($1,$2,$3,$4) RETURNING *",
+    [name, date, progress, mood]
+  );
+  return data.rows;
+}
+
+export async function updateById(name, date, progress, mood, id) {
+  const data = await query(
+    "UPDATE workshopTracker SET (name, date, progress, mood) = ($1,$2,$3,$4) WHERE id = $5  RETURNING *",
+    [name, date, progress, mood, id]
+  );
+  return data.rows;
+}
+
+export async function deleteById(id) {
+  const data = await query("DELETE FROM workshopTracker WHERE id = $1", [id]);
+  return data.rows;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,43 @@
 import express from "express";
 const router = express.Router();
 
-import { getAllData } from "../models/workshopTracker.js";
+import {
+  getAllData,
+  getById,
+  postWorkshop,
+  deleteById,
+  updateById,
+} from "../models/workshopTracker.js";
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   const data = await getAllData();
   res.json({ success: true, payload: data });
+});
+
+router.get("/:id", async function (req, res, next) {
+  const { id } = req.params;
+  const data = await getById(id);
+  res.json({ success: true, payload: data });
+});
+
+router.post("/", async function (req, res, next) {
+  const { name, date, progress, mood } = req.body;
+  const data = await postWorkshop(name, date, progress, mood);
+  res.json({ success: true, payload: data });
+});
+
+router.patch("/:id", async function (req, res, next) {
+  const { id } = req.params;
+  const { name, date, progress, mood } = req.body;
+  const data = await updateById(name, date, progress, mood, id);
+  res.json({ success: true, payload: data });
+});
+
+router.delete("/:id", async function (req, res, next) {
+  const { id } = req.params;
+  const deletedData = await deleteById(id);
+  res.json({ success: true, payload: deletedData });
 });
 
 export default router;
